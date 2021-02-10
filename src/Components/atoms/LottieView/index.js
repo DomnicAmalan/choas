45927eeefba01ac396325a2a8adc10bd72af6431
amalandomnic@gmail.com <domnic.amalan@gitstart.dev>
2021-02-10 10:22:58 +0530

    Home Page Completed

--- src/Components/atoms/LottieView/index.js
+++ src/Components/atoms/LottieView/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Lottie from 'react-lottie';
 import * as animationData from '../../../assets/logo.json'
 
-const LottieView = ({}) => {
+const LottieView = () => {
   const defaultOptions = {
     loop: true,
     autoplay: true, 
