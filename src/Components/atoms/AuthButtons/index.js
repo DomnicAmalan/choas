97b29683097a86e747491c9309d43efe2f676b71
JB <joishbosco99@gmail.com>
2021-02-12 22:55:39 +0530

    perf : social login using firebase

--- src/Components/atoms/AuthButtons/index.js
+++ src/Components/atoms/AuthButtons/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './authbutton.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const AuthButtons = ({ title, icon, style }) => (
-  <div className="auth-button-container">
-    <FontAwesomeIcon style={style} className="icon" icon={icon} />
+const AuthButtons = ({ title, icon, style, action }) => (
+  <div className="auth-button-container" onClick={action}>
+    <FontAwesomeIcon style={style} className="icon" icon={icon}/>
     <p className="auth-button-text">{title}</p>
   </div>
 );
